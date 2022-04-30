86ceac21fdd2b7a4849af28350642e9d9f5249a1
aitegin <zhanybaev1211@gmail.com>
2022-04-30 15:15:25 +0600

    Show rating

--- src/components/Product/ProductDetail/ProductDetail.tsx
+++ src/components/Product/ProductDetail/ProductDetail.tsx
@@ -6,10 +6,6 @@ import { getDetail } from '../../../store/actions/product.action';
 import { IState } from '../../../types/products.types';
 import './ProductDetail.css'
 
-const colors = {
-    orange: '#FFBA5A',
-    grey: '#a9a9a9'
-}
 
 const ProductDetail = (): JSX.Element => {
     const {id}:any = useParams()
@@ -17,10 +13,40 @@ const ProductDetail = (): JSX.Element => {
     
     const dispatch = useDispatch()
 
+    let stars = Array(5).fill(0)
+
+    const [currentValue, setCurrentValue] = useState(0);
+    const [hoverValue, setHoverValue ] = useState();
+
+    const handleClick = (value:any) => {
+        setCurrentValue(value)
+    }
+
+    const handleMouseOver = (value: any) => {
+        setHoverValue(value)
+    }
+
+    const handleMouseLeave = () => {
+        setHoverValue(undefined)
+    }
+
+    const ratingDisplay = (arr:number[] | null) =>{
+        let sum = 0
+        if(arr && arr.length){
+            arr.forEach((elem) => {
+                sum+=elem
+            });
+        }
+        return arr ? Math.floor(sum/arr.length) : 1;
+    }
 
     useEffect(()=>{
         getDetail(dispatch, id)
     }, [])
+
+    if(product){
+        console.log(ratingDisplay(product.rating));
+    }
 
     return (
         <React.Fragment>
@@ -28,8 +54,42 @@ const ProductDetail = (): JSX.Element => {
                 product ? (
                     <div style={{paddingTop:'100px'}}>
                         <img src={product.img} alt="" />
+                        <div>
+                            <span>Rating:</span>
+                            {
+                                stars.map((item, index)=>(
+                                    <FaStar 
+                                        key={index} 
+                                        size={24}
+                                        style={{
+                                            marginRight:10,
+                                            cursor:'pointer'
+                                        }}
+                                        color={(ratingDisplay(product.rating)) > index ? '#FFBA5A' : '#a9a9a9' }
+                                    />
+                                ))
+                            }
+                        </div>
                         <h4>{product.title}</h4>
-                        <input type="number" name="rating" max={5} min={1} />
+                        <div className="stars">
+                            {
+                                stars.map((item, index)=>(
+                                    <FaStar 
+                                        key={index} 
+                                        size={24}
+                                        style={{
+                                            marginRight:10,
+                                            cursor:'pointer'
+                                        }}
+                                        color={(hoverValue || currentValue) > index ? '#FFBA5A' : '#a9a9a9' }
+                                        onClick={() => handleClick(index + 1)}
+                                        onMouseOver={() => handleMouseOver(index + 1)}
+                                        onMouseLeave={handleMouseLeave}
+                                    />
+                                ))
+                            }
+                        </div>
+                        <button>Submit</button>
                     </div>
                 ) :
                 <h2>loading</h2>
